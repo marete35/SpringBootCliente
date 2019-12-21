6f01d7ebbb0fa9ef42cb8f5e1044bcf3b1ab33a3
Mario Galarza <mgalarza@learsoft.com.ar>
2019-12-20 22:28:39 -0300

    first commit

---
+++ src/main/java/ar/com/marete/Basic/SpringBootClienteApplication.java
@@ -0,0 +1,13 @@
+package ar.com.marete.Basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClienteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ar/com/marete/Basic/servicios/PersonaController.java
@@ -0,0 +1,17 @@
+package ar.com.marete.Basic.servicios;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/personas")
+public class PersonaController {
+	
+	
+	@RequestMapping("/lista")
+	public String mostrarPersonas(Model modelo) {
+		return "personas/lista";
+	}
+	
+}
